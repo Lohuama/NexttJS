6b4b44182234fc39a23deca9571d828a47e0087f
Lohuama <limalorruama98@gmail.com>
2022-12-20 13:01:18 -0300

    1:07

--- events-app/pages/index.js
+++ events-app/pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({title}) {
+export default function Home({data}) {
   return (
     <>
       <Head>
@@ -25,27 +25,14 @@ export default function Home({title}) {
       </header>
 
       <main className={styles.main}>
-        <a href='/events/london'>
-          <img />
-          <h2>Events in London</h2>
-          <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </a>
-        <a href='/events/sanfran'>
-          <img />
-          <h2>Events in San Francisco</h2>
-          <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </a>
-        <a href='/events/barcelona'>
-          <img />
-          <h2>Events in Barcelona</h2>
-          <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </a>
+        {data.map((ev) => (
+          <a key={ev.id} href={`/events/${ev.id}`}> 
+              <Image width={200} height={100} alt={ev.title} src={ev.image} /> 
+              <h2> {ev.title} </h2> 
+              <p> {ev.description} </p> 
+          </a>
+        ))}
+
       </main>
 
       <footer className={styles.footer}>
@@ -55,10 +42,14 @@ export default function Home({title}) {
   )
 }
 
-export function getServerSideProps(){
+export async function getServerSideProps(){
+  const {events_categories} = await import('/data/data.json');
+  // const res = await fetch('URL');
+  // const data = await res.json();
+  console.log({events_categories})
   return {
       props:{
-          title: 'Hello Everyone!',
+          data: events_categories
       },
   }
 }
